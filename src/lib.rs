2c4b50b136e50fb1226f1a1c2abd3556b22c176a
Conturing <13372078+Conturing@users.noreply.github.com>
2019-05-24 09:50:46 +0200

    Define Wavelet Tree Trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,63 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate bv;
+use bio::data_structures::rank_select::RankSelect;
+use bv::BitVec;
+use bv::BitsMut;
+
+
+struct PointerWaveletTree<T> {
+    e: T
+}
+
+impl<T> PointerWaveletTree<T> {
+
+    fn new() {
     }
 }
+
+impl<T> WaveletTree<T> for PointerWaveletTree<T> {
+
+    fn access(&self, index: u32) {
+    
+    }
+
+    fn rank(&self, element: T, index: u32) {
+    
+    }
+
+    fn select(&self, element: T, index: u32) {
+    
+    }
+}
+
+
+
+trait WaveletTree<T> {
+
+    fn access(&self, index: u32);
+
+    fn rank(&self, element: T, index: u32);
+
+    fn select(&self, element: T, index: u32);
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn access() {
+    
+    }
+
+    #[test]
+    fn rank() {
+    
+    }
+
+    #[test]
+    fn select() {
+    
+    }
+}
+
+
