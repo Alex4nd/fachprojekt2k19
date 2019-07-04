598f560e08b7d5856dee5a744e7d9cf299ce7774
Conturing <13372078+Conturing@users.noreply.github.com>
2019-07-04 15:14:55 +0200

    Update Graphs.

--- src/graph.rs
+++ src/graph.rs
@@ -20,43 +20,150 @@ use crate::traits::Node;
 use crate::pointer::PointerWaveletTree;
 
 
-struct Graph {
+pub struct Graph {
     tree: PointerWaveletTree<u64>,
     bits: BitVec<u8>,
+    max_node: u64,
 }
 
 impl Graph {
 
-    pub fn new<T>(adjacency: &Vec<(T,T)>) -> Graph 
-        where T: Eq + Hash {
-        
-        let mut set: HashMap<&T, (u64,Vec<&T>)> = HashMap::new();
-        let mut index = 0;
-        for (a,b) in adjacency.iter() {
-            if !set.contains_key(a) {
-                set.insert(a, (index, Vec::new()));
-                index += 1;
+    pub fn new_tuple(tuples: &Vec<(u64,u64)>) -> Graph {
+        let mut set: HashMap<u64, Vec<u64>> = HashMap::new();
+        let mut max = 0; 
+
+        for (a,b) in tuples.iter() {
+            if *a > max {
+                max = *a;
             }
-            let (idx, ref mut list) = set.get_mut(a).unwrap();
-            list.push(b);
+            if *b > max {
+                max = *b;
+            }
+            if !set.contains_key(a) {
+                set.insert(*a, Vec::new());
+            }
+            if !set.contains_key(b) {
+                set.insert(*b, Vec::new());
+            }
+            let ref mut list = set.get_mut(a).unwrap();
+            list.push(*b);
         }
+        return Graph::new(&set, max); 
+    }
 
+    pub fn new(adjacency: &HashMap<u64, Vec<u64>>, max_node: u64) -> Graph {
         let mut data = Vec::new();
+        let mut bits = BitVec::new();
 
-        for (key, (idx,list)) in set.iter() {
-            for elem in list.iter() {
-                let (idx, l) = set.get(elem).unwrap();
-                data.push(*idx);
+        for key in 0..max_node+1 {
+            bits.push(true);
+            let list = adjacency.get(&key);
+            if list.is_none() {
+                continue;
+            }
+
+            for elem in list.unwrap().iter() {
+                data.push(*elem);
+                bits.push(false);
             }
         }
 
         let tree = PointerWaveletTree::new_fill(&data);
-        let bits = BitVec::new();
 
         Graph {
             tree,
             bits,
+            max_node,
         }
     }
 
+    pub fn neighbour(&self, node: u64, i: u64) -> Option<u64> {
+        if node > self.max_node {
+            return None;
+        }
+
+        let rs = RankSelect::new(self.bits.clone(), 1);
+        let k = rs.select_1(node + 1).unwrap();
+
+        println!("RS select {}", k);
+
+        return self.tree.access((k - node + i - 1) as u32);
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::pointer::*;
+    use crate::traits::WaveletTree;
+
+    #[test]
+    fn constructor_wavelet_tree_correct() {
+
+        let data = vec!((1,2), (2,3), (3,4));
+
+        let mut g = Graph::new_tuple(&data);
+
+        let mut bits = vec!(2,3,4);
+        assert_eq!(g.tree.to_vec(), Some(bits));
+    }
+
+    #[test]
+    fn constructor_maxnode_correct() {
+
+        let data = vec!((1,2), (2,3), (3,4));
+
+        let mut g = Graph::new_tuple(&data);
+
+        assert_eq!(g.max_node, 4)
+    }
+    
+    #[test]
+    fn constructor_bitvector_correct() {
+
+        let data = vec!((1,2), (2,3), (3,4));
+
+        let mut g = Graph::new_tuple(&data);
+
+        let mut pattern = vec!(1,1,0,1,0,1,0,1);
+        assert_bit_pattern(&mut g.bits, &pattern);
+    }
+
+    fn assert_bit_pattern(bits: &mut BitVec<u8>, pattern: &Vec<u32>) {
+        println!("Bitvec: {:?}", bits);
+        let mut i = 0;
+        assert_eq!(bits.len() as usize, pattern.len());
+        for bit in pattern.iter() {
+            assert_eq!(bits.get(i), *bit == 1, "Bit {} should be {}", i, *bit);
+            i += 1;
+        }
+    }
+      
+    #[test]
+    fn neighbour_single_successor() {
+
+        let data = vec!((1,2), (2,3), (3,4));
+
+        let mut g = Graph::new_tuple(&data);
+
+        assert_eq!(g.neighbour(1, 1), Some(2), "1st neighbour of node 1");
+        assert_eq!(g.neighbour(2, 1), Some(3), "1st neighbour of node 2");
+        assert_eq!(g.neighbour(3, 1), Some(4), "1st neighbour of node 3");
+    }  
+    
+    #[test]
+    fn neighbour_cycle() {
+
+        let data = vec!((1,2), (2,3), (3,4), (4,1), (1,5), (1,6));
+
+        let mut g = Graph::new_tuple(&data);
+
+        assert_eq!(g.neighbour(1, 1),Some(2), "1st neighbour of node 1");
+        assert_eq!(g.neighbour(1, 2),Some(5), "2nd neighbour of node 1");
+        assert_eq!(g.neighbour(1, 3),Some(6), "3rd neighbour of node 1");
+        assert_eq!(g.neighbour(2, 1),Some(3), "1st neighbour of node 2");
+        assert_eq!(g.neighbour(3, 1),Some(4), "1st neighbour of node 3");
+        assert_eq!(g.neighbour(4, 1),Some(1), "1st neighbour of node 4");
+    }  
 }
